6263fc7ade6bd7ba97bc76d66a888a8f34bcac93
Zachary Piontek <zakpiontek@gmail.com>
2022-11-16 15:42:59 -0800

    oddish-evenish function works

---
+++ day-01/challenges/PRIM02-oddish-evenish.js
@@ -0,0 +1,10 @@
+function oddishOrEvenish(number) {
+  number = number
+    .toString()
+    .split('')
+    .map(num => {return Number(num);})
+    .reduce((a, b) => {return a + b;});
+  return number % 2 === 0 ? 'Evenish' : 'Oddish';
+}
+
+console.log(oddishOrEvenish(2445));
